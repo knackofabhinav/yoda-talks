d9b7a85878daa1e016680ea9b00d24300e01092a
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2020-12-02 18:23:09 +0530

    error handling

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ var btnTranslate = document.querySelector("#btn-translate")
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="
 var serverURL = "https://api.funtranslations.com/translate/yoda.json?text="
 
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Server Takes Only 5 requests per hour so try again after sometime. Sorry :(")
+}
+
 btnTranslate.addEventListener("click",
 function callback(){
     fetch(serverURL + inputText.value)
     .then(res => res.json())
-    .then(json => outputText.innerText = json.contents.translated)  
+    .then(json => outputText.innerText = json.contents.translated) 
+    console.log("clicked")
+    .catch(errorHandler)
 })
